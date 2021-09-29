ce9b8bb9970aeff0984b30b1c3aff9a5de8e204a
Sayansree <sayansreeparia@gmail.com>
2021-09-29 19:56:29 +0530

    origin main
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- js/newsletter.js
+++ js/newsletter.js
@@ -6,6 +6,36 @@ window.onload=()=>{
             $(`.lang`).hide()
             $(`.${event.target.value}`).show()
         }
+        const searchPeople=$("#search-input")[0]
+        searchPeople.onkeyup =()=>{
+            let querry= searchPeople.value
+            // if(querry.length<3)return
+            fetch('https://it-connects-us.herokuapp.com/search',
+                {
+                    method:'post',
+                    mode:'cors',
+                    body : JSON.stringify({'querry':querry,'limit':5}),
+                    headers: {"Content-type": "application/json; charset=UTF-8"},
+                }
+                ).then((resp)=>resp.json())
+                .then((resp)=>{
+                    let dl=$("#people")[0]
+                    dl.innerHTML=""
+                    if(resp.ok){
+                        
+                        for( k of resp.data){
+                            dl.innerHTML+=`<option value="${k.name}">`
+                        }
+                      
+                        // msg.style.color="green"
+                        // msg.innerHTML= `successfully subscribed`;
+                    }else{
+                        dl.innerHTML=`<option  value="No results found">`
+                        // msg.style.color="orange"
+                        // msg.innerHTML= "you have already subscribed!";
+                    }
+                })
+        }
 
         $("#news-btn").onclick=async()=>{
             const msg=document.getElementById("news-msg");
